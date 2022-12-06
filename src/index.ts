a0cf0e61f081b4615160f6ed664b7ada2681e4a2
Lesenya Mokhubu <lsmokhubu@gmail.com>
2022-12-06 18:19:49 +0200

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,20 @@
-/**
- * Required External Modules
- */
 import * as dotenv from "dotenv";
 import express from "express";
 import { router } from "./routes/credit-card.route";
 
-/**
- * App Variables
- */
-
 dotenv.config();
+
 if (!process.env.PORT) {
     process.exit(1);
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+const port: number = parseInt(process.env.PORT as string, 10);
 const app = express();
-
-/**
- *  App Configuration
- */
 
 app.use(express.json());
 
-/**
- * Server Activation
- */
-
 app.use("/api/validate/", router);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
