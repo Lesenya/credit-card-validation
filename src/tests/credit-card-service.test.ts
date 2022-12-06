a28973b7aa958a8dd031d534d6656fc2936d6071
Lesenya Mokhubu <lsmokhubu@gmail.com>
2022-12-06 15:26:34 +0200

    Add dockerfile and docker-compose

--- src/tests/credit-card-service.test.ts
+++ src/tests/credit-card-service.test.ts
@@ -2,8 +2,10 @@ import 'jest';
 import { isAValidCreditCard } from '../services/credit-card.service';
 
 describe("Credit card validation", () => {
+
     it("Should return false for invalid credit card numbers", async () => {
         const invalidCreditCardNumbers: string[] = ['49927398717', '1234567812345678'];
+
         invalidCreditCardNumbers.forEach(cardNumber => {
             expect(isAValidCreditCard(cardNumber)).toBe(false);
         });
@@ -11,8 +13,10 @@ describe("Credit card validation", () => {
 
     it("Should return true for valid credit card numbers", async () => {
         const validCreditCardNumbers: string[] = ['49927398716', '1234567812345670', '2222405343248877', '2222990905257051'];
+
         validCreditCardNumbers.forEach(cardNumber => {
             expect(isAValidCreditCard(cardNumber)).toBe(true);
         });
     });
+    
 });
